847b2158d752de3733059d7aaf7e1d54bab3d4bc
jh <wlguni@dgu.ac.kr>
2022-07-05 21:43:23 +0900

    220705

---
+++ jh/lesson600/11724_.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10000)
+# 최대 재귀 횟수 제한 제거
+
+def dfs(v):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n+1):
+    if visited[i] == 0:
+        cnt += 1
+        dfs(i)
+
+print(cnt)
