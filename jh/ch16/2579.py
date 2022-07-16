59c0e1bc9710d3fb32b04380cbba4d8ee010d75e
jh <wlguni@dgu.ac.kr>
2022-07-16 12:03:59 +0900

    220716

---
+++ jh/ch16/2579.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0] * n
+A = [0] * n
+for i in range(n):
+    A[i] = int(input())
+
+dp[0] = A[0]
+dp[1] = A[0]+A[1]
+dp[2] = max(A[1]+A[2], A[0]+A[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-3]+A[i-1]+A[i], dp[i-2]+A[i])
+
+print(dp[n-1])
