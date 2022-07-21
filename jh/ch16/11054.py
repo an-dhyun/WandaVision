b330d78221c11583a317009365118f38b5901735
jh <wlguni@dgu.ac.kr>
2022-07-21 17:03:01 +0900

    220721

---
+++ jh/ch16/11054.py
@@ -0,0 +1,20 @@
+N = int(input())
+A = list(map(int, input().split()))
+dpi = [1] * N # increase
+dpd = [1] * N # decrease
+dp = [0] * N # sum
+
+for i in range(1, N):
+    for j in range(i):
+        if A[i] > A[j] and dpi[i] < dpi[j]:
+            dpi[i] = max(dpi[j]+1, dpi[i])
+
+for i in range(N-2, -1, -1):
+    for j in range(N-1, i, -1):
+        if A[i] > A[j]:
+            dpd[i] = max(dpd[j]+1, dpd[i])
+
+for i in range(N):
+    dp[i] = dpi[i] + dpd[i] - 1
+
+print(max(dp))
