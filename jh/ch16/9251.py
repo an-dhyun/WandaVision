b7df8490f8bf8a37a8571d968d558b8d0df95530
jh <wlguni@dgu.ac.kr>
2022-07-23 14:05:13 +0900

    220723

---
+++ jh/ch16/9251.py
@@ -0,0 +1,12 @@
+A = list(input())
+B = list(input())
+
+dp = [[0] * (len(A)+1) for _ in range(len(B)+1)]
+for i in range(len(B)):
+    for j in range(len(A)):
+        if A[j] == B[i]:
+            dp[i+1][j+1] = dp[i][j] + 1
+        else:
+            dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+
+print(dp[-1][-1])
