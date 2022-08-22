50ef7b32126032987dcd7e04ecc212081b105d1c
an-dhyun <god2000sus@naver.com>
2022-08-22 18:14:49 +0900

    220822

--- dh/lesson521/1339.py
+++ dh/lesson521/1339.py
@@ -1,30 +1,31 @@
 import sys
+import operator 
 
-n = int(sys.stdin.readline())
+# 값 받아오기
+N = int(sys.stdin.readline())
+words = []
+for i in range(N):
+    words.append(sys.stdin.readline().strip())
 
-alpha = [] # 단어를 저장할 리스트
-alpha_dict = {} # 단어 내의 알파벳별로 수를 저장할 딕셔너리
-numList = [] # 수를 저장할 리스트
+# 최대자릿수 계산
+alph = {}
+maxlen = 0
+for i in words:
+    if len(i) >= maxlen: maxlen = len(i)
 
-for i in range(n): # 단어를 입력받음
-    alpha.append(sys.stdin.readline().rstrip())
+# 자릿값 계산
+for i in words:
+    for j in range(len(i)):
+        if i[j] not in alph:
+            alph[i[j]] = 10**(len(i)-j-1) # 자릿수만큼 반환
+        else:
+            alph[i[j]] += 10**(len(i)-j-1)
+alph = dict(sorted(alph.items(), key=lambda x: x[1], reverse=True))
 
-for i in range(n): # 모든 단어에 대해서
-    for j in range(len(alpha[i])): # 단어의 길이만큼 실행
-        if alpha[i][j] in alpha_dict: # 단어의 알파벳이 이미 dict에 있으면
-            alpha_dict[alpha[i][j]] += 10 ** (len(alpha[i])-j-1) # 자리에 맞게 추가 ( 1의 자리면 1 )
-        else:   # 자리에 없으면 새로 추가 ( 10의 자리면 10 )
-            alpha_dict[alpha[i][j]] = 10 ** (len(alpha[i])-j-1)
-
-for val in alpha_dict.values(): # dict에 저장된 수들을 모두 리스트에 추가
-    numList.append(val)
-
-numList.sort(reverse=True) # 수들을 내림차순 정렬
-
-sum = 0
-pows = 9
-for i in numList: # 첫 번째 부터 가장 큰 부분을 차지하므로 9를 곱해준다
-    sum += pows * i
-    pows -= 1
-# 내려갈수록 그 알파벳이 차지하는 비중이 적으므로 -1 
-print(sum)
+# 합 계산
+result = 0
+num = 9
+for i in alph:
+    result += alph[i]*num
+    num -= 1
+print(result)
